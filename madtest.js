0a6a9d9b3a59a566afd91c89b2e0c4be80e074f8
Jayla (they/them) <jakeclegg99@gmail.com>
2024-09-14 13:01:03 +0100

    Added notes
    Added my notes as comments so i can keep check in what i am doing

--- madtest.js
+++ madtest.js
@@ -1,4 +1,17 @@
 
+/*List of things to do
+Fetch from each page
+search page for  title “Short Stories” by the author “Dostoyevsky, Theodor”
+if not there go to next page
+
+output book
+
+USE THESE IN CODE
+using the array.sort method 
+Using array.map, modify each item’s item.subjects[] entries to be uppercase.
+Using array.filter, remove all entries whose author didn’t exist within the last 200 years.
+*/
+
 let pagenum = 1;
 console.log("Searching page " + pagenum + " of the index")
 
